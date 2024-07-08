97c8fa17b02948fe57e95a39a92113d03a992bf1
Fernanda Guimarães Santana <fernandaguimaraes0202@gmail.com>
2024-07-08 16:45:30 -0300

    ajuste página linguagens

--- src/components/accordion/linguagens.tsx
+++ src/components/accordion/linguagens.tsx
@@ -1,21 +1,63 @@
+"use client";
+
 import React from "react";
 import Accordion from "@/components/accordion";
+import dynamic from 'next/dynamic';
+
+const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 const accordionLinguagens = [
   {
     title: "PORTUGUÊS",
     submenus: [
       { content: "Interpretação de texto- verbal e não verbal" },
-      { content: "Interpretação de texto- verbal e não verbal" },
-      { content: "Interpretação de texto- verbal e não verbal" },
+      { content: "Gêneros textuais " },
+      { content: "Variação Linguística" },
+      { content: "Figuras de Linguagem" },
+      { content: "Intertextualidade" },
+      { content: "Redação " },
+    ],
+  },
+  {
+    title: "INGLÊS",
+    submenus: [
+      { content: "Tempos verbais " },
+      { content: " Voz passiva" },
+      { content: "Pronomes pessoais" },
+      { content: "Linking words" },
+    ],
+  },
+  {
+    title: "ESPANHOL",
+    submenus: [
+      { content: "Interpretação e compreensão textual;" },
+      { content: "História da hispanidade;" },
+      { content: "Semântica e léxico;" },
+      { content: "Funções do texto" },
+      { content: "Tirinhas e charge." },
     ],
   },
 ];
 
 export function LinguagensConteudo() {
+
   return (
-    <>
-      <Accordion items={accordionLinguagens} />
-    </>
+    <div className="linguagens">
+      <h3 className="h3-title">Linguagens</h3>
+      <h4 className="h4-title">Principais Tópicos</h4>
+      <div>
+        <Accordion items={accordionLinguagens} />
+      </div>
+      <div>
+        <h4 className="h4-title">Dicas</h4>
+        <div className="video">
+          <center>
+            <ReactPlayer url={'https://www.youtube.com/watch?v=TuBKsV3lSQc'} />
+          </center>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default LinguagensConteudo;
